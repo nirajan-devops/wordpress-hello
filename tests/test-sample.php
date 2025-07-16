a81e479a8e75c9a9e939ec521de10e55a791367e
nirajan <nirvananire@gmail.com>
2025-07-17 00:38:07 +0545

    Adding few more updates

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,13 +1,18 @@
 <?php
 
+namespace YourProject\Tests;
+
 use PHPUnit\Framework\TestCase;
 
-class SampleTest extends TestCase {
-    public function testTrueIsTrue() {
+class SampleTest extends TestCase
+{
+    public function testTrueIsTrue()
+    {
         $this->assertTrue(true);
     }
-    
-    public function testWordPressLoaded() {
+
+    public function testWordPressFunctionsAvailable()
+    {
         $this->assertTrue(function_exists('add_action'));
     }
 }
